7467ff11749b7ffaad52cda67130fd6992bd0542
unknown <grey7@ptc-grey-07.ptc.local>
2016-06-06 11:38:15 +0300

    Initial commit

---
+++ src/main/java/MovieLand/App.java
@@ -0,0 +1,13 @@
+package MovieLand;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
