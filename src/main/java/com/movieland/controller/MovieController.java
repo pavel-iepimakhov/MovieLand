eaba33e67b9cc106787e2bbf86d65f856012abb3
pavel-iepimakhov <justletmeinemail@gmail.com>
2016-06-17 10:44:36 +0300

    added implementation for [lg-1] Add file appender

--- src/main/java/com/movieland/controller/MovieController.java
+++ src/main/java/com/movieland/controller/MovieController.java
@@ -3,6 +3,8 @@ package com.movieland.controller;
 import com.movieland.entity.Movie;
 import com.movieland.service.*;
 import com.movieland.util.JsonConverterService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,10 @@ import java.util.List;
 
 @Controller
 public class MovieController {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
+    long startTime;
+
     @Autowired
     private MovieService movieService;
 
@@ -21,7 +27,17 @@ public class MovieController {
     @RequestMapping(value = "/v1/movies", produces = "application/json;charset=UTF-8")
     @ResponseBody
     public String getAllMovies(){
+        if(LOGGER.isDebugEnabled()) {
+            startTime = System.nanoTime();
+            LOGGER.debug("getAllMovies started execution");
+        }
+
         List<Movie> movies = movieService.getAllMovies();
+
+        if(LOGGER.isDebugEnabled()) {
+            startTime = System.nanoTime();
+            LOGGER.debug("getAllMovies finished execution. Elapsed time is {}",System.nanoTime() - startTime);
+        }
         return jsonConverterService.objectToJson(movies);
     }
 
