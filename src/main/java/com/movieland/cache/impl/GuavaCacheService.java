07772c944e22abb08c39a40af1c95f74377c0c22
pavel-iepimakhov <justletmeinemail@gmail.com>
2016-06-27 11:57:07 +0300

    GDS

---
+++ src/main/java/com/movieland/cache/impl/GuavaCacheService.java
@@ -0,0 +1,79 @@
+package com.movieland.cache.impl;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.movieland.controller.MovieController;
+import com.movieland.entity.Genre;
+import com.movieland.cache.CacheService;
+import com.movieland.service.MovieService;
+import com.movieland.util.CurrencyEnum;
+import com.movieland.util.CurrencyExchangeRateService;
+import com.movieland.util.ExchangeRate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+// TODO: 16.06.2016 implement custom cache
+@Service
+public class GuavaCacheService implements CacheService {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(GuavaCacheService.class);
+
+    @Autowired
+    private MovieService movieService;
+
+    @Autowired
+    private CurrencyExchangeRateService currencyExchangeRateService;
+
+    private LoadingCache<Integer, List<Genre>> movieGenresCache =
+            CacheBuilder.newBuilder()
+                    .expireAfterWrite(4, TimeUnit.HOURS)
+                    .build(new CacheLoader<Integer, List<Genre>>() {
+                        @Override
+                        public List<Genre> load(Integer movieId) throws Exception {
+                            return movieService.getGenresByMovieId(movieId);
+                        }
+                    });
+
+    private LoadingCache<CurrencyEnum, ExchangeRate> currencyExchangeRateCache =
+            CacheBuilder.newBuilder()
+                    .expireAfterWrite(10, TimeUnit.MINUTES)
+                    .build(new CacheLoader<CurrencyEnum, ExchangeRate>() {
+                        @Override
+                        public ExchangeRate load(CurrencyEnum currencyEnum) throws Exception {
+                            return currencyExchangeRateService.fetchCurrencyExchangeRate(currencyEnum);
+                        }
+                    });
+
+    @Override
+    public List<Genre> getMovieGenres(int movieId) {
+        try {
+            return movieGenresCache.get(movieId);
+        } catch (ExecutionException e) {
+            return movieService.getGenresByMovieId(movieId);
+        }
+    }
+
+    @Override
+    public ExchangeRate getExchangeRateForCurrency(CurrencyEnum currency) {
+        try{
+            ExchangeRate rate = currencyExchangeRateCache.get(currency);
+            return rate;
+        } catch (ExecutionException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public void invalidateAllCaches() {
+        LOGGER.info("Invalidating all caches...");
+        movieGenresCache.invalidateAll();
+        currencyExchangeRateCache.invalidateAll();
+    }
+}
---
+++ src/main/java/com/movieland/entity/dto/MovieWithUserRatingDTO.java
@@ -0,0 +1,125 @@
+package com.movieland.entity.dto;
+
+import com.movieland.entity.Country;
+import com.movieland.entity.Genre;
+import com.movieland.entity.MovieReview;
+
+import java.util.List;
+
+public class MovieWithUserRatingDTO {
+    private int movieId;
+    private String movieNameEng;
+    private String movieNameRus;
+    private int movieYear;
+    private double movieRating;
+    private double moviePrice;
+    private String movieDescription;
+    private List<MovieReview> reviews;
+    private List<Genre> genres;
+    private List<Country> countries;
+    private float userRating;
+
+    public float getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(float userRating) {
+        this.userRating = userRating;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getMovieNameEng() {
+        return movieNameEng;
+    }
+
+    public void setMovieNameEng(String movieNameEng) {
+        this.movieNameEng = movieNameEng;
+    }
+
+    public String getMovieNameRus() {
+        return movieNameRus;
+    }
+
+    public void setMovieNameRus(String movieNameRus) {
+        this.movieNameRus = movieNameRus;
+    }
+
+    public int getMovieYear() {
+        return movieYear;
+    }
+
+    public void setMovieYear(int movieYear) {
+        this.movieYear = movieYear;
+    }
+
+    public double getMovieRating() {
+        return movieRating;
+    }
+
+    public void setMovieRating(double movieRating) {
+        this.movieRating = movieRating;
+    }
+
+    public double getMoviePrice() {
+        return moviePrice;
+    }
+
+    public void setMoviePrice(double moviePrice) {
+        this.moviePrice = moviePrice;
+    }
+
+    public String getMovieDescription() {
+        return movieDescription;
+    }
+
+    public void setMovieDescription(String movieDescription) {
+        this.movieDescription = movieDescription;
+    }
+
+    public List<MovieReview> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<MovieReview> reviews) {
+        this.reviews = reviews;
+    }
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<Genre> genres) {
+        this.genres = genres;
+    }
+
+    public List<Country> getCountries() {
+        return countries;
+    }
+
+    public void setCountries(List<Country> countries) {
+        this.countries = countries;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "movieId=" + movieId +
+                ", movieNameEng='" + movieNameEng + '\'' +
+                ", movieNameRus='" + movieNameRus + '\'' +
+                ", movieYear=" + movieYear +
+                ", movieRating=" + movieRating +
+                ", moviePrice=" + moviePrice +
+                ", movieDescription='" + movieDescription + '\'' +
+                ", reviews=" + reviews +
+                ", genres=" + genres +
+                ", countries=" + countries +
+                '}';
+    }
+}
